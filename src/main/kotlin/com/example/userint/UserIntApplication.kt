e74dc9748f5ec66158045096dabedf394a053457
Cristian Escujuri <cristianescujuri@MBP-de-Cristian.fibertel.com.ar>
2023-11-19 22:39:26 -0300

    -

--- src/main/kotlin/com/example/userint/UserIntApplication.kt
+++ src/main/kotlin/com/example/userint/UserIntApplication.kt
@@ -2,8 +2,10 @@ package com.example.userint
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 
 @SpringBootApplication
+@EnableWebSocketMessageBroker
 class UserIntApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/userint/v1/WebSocketController.kt
+++ src/main/kotlin/com/example/userint/v1/WebSocketController.kt
@@ -1,5 +1,7 @@
 package com.example.userint.v1
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
@@ -12,11 +14,14 @@ class WebSocketController{
     @Autowired
     private lateinit var messagingTemplate: SimpMessageSendingOperations
 
+    private val logger: Logger = LoggerFactory.getLogger(WebSocketController::class.java)
+
     @MessageMapping("/ws")
     @SendTo("/topic/update")
     fun sendUpdate(message: String): String {
-
         val notification = "Nueva notificación: $message"
+        logger.info("Mensaje recibido: {}", message)
+        logger.info("Enviando notificación: {}", notification)
         messagingTemplate.convertAndSend("/topic/update", notification)
         return notification
     }
